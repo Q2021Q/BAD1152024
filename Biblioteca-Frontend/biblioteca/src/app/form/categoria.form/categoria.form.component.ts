6523f3f506a7c8bad0b1d94e5030f6b0a82d0e7c
stive-dev <jonathanlima04@hotmail.com>
2024-06-08 17:11:17 -0600

    Categorias working CRUD

--- Biblioteca-Frontend/biblioteca/src/app/form/categoria.form/categoria.form.component.ts
+++ Biblioteca-Frontend/biblioteca/src/app/form/categoria.form/categoria.form.component.ts
@@ -1,21 +1,35 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-categoria.form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './categoria.form.component.html',
   styleUrl: './categoria.form.component.css'
 })
 export class CategoriaFormComponent {
+  public categorias: any;
   public categoria: any = {
     nombreCategoria: '',
     descripcionCategoria: ''
   }
 
   async ngOnInit() {
-    const res = await fetch("")
+    this.fetchCategorias();
+  }
+
+  async fetchCategorias() {
+    const token: string = localStorage.getItem("JWT")!;
+    const res = await fetch("http://localhost:8080/bibliotecario/getAllCateRecur", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token
+      }});
+
+      this.categorias = await res.json();
   }
 
   async agregarCategoria() {
@@ -29,5 +43,30 @@ export class CategoriaFormComponent {
       },
       body: JSON.stringify(this.categoria)
     })
+
+    this.fetchCategorias();
+  }
+
+  async modificarCategoria(categoria: any) {
+    this.categoria = categoria;
+  }
+
+  async cambiarCategoria() {
+    const token: string = localStorage.getItem("JWT")!;
+    //const token: string = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZSI6IkJJQlVTT05MIiwiZXhwIjoxNzE3ODkxNjM3LCJpYXQiOjE3MTc4NzcyMzd9.bdWkFMq20EZoEZ_CxhBtyrQDQ7wuw3sWV4Pbw8BRLyA2AiUpNzSlhF6qv4_bxugeFDeglzqJ36KJVYxCS9NFIWi0vNTovFr1N5yrV5BI4SNyZQXD4-PpMQYRst72Wr0wDE15Lco9Bge-ac-Y483vfijVUr2yy1u7glehjWOCwY1BGN0qv2bt4_0Mj8gnSxff7bWKA6olK8nLpUdouTYLl_f5UrGSBoYKOOCaDyIOnmksC9EAIgRIVEW-2kO_mnrgJZy2QNVq31CRKPvdqaQmWL_4ldkP8e0ETJJPTW7NtvM2_GlwGI48x9Q_OVqEn1ToGo24SUo16RQ2uM3LMzdTUw"
+    const res = await fetch("http://localhost:8080/bibliotecario/updatecategoriarecurso", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token
+      },
+      body: JSON.stringify(this.categoria)
+    })
+
+    console.log(await res.text())
+  }
+
+  async borrarCategoria(categoria: any) {
+    
   }
 }
